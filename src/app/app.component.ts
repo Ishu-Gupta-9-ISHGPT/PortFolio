bda378a5a6d9994515033fc7e53dd973b46689cd
ishu gupta <guptarshu999@gmail.com>
2025-03-02 14:14:37 +0530

    Revert "Merge pull request #1 from Ishu-Gupta-9-ISHGPT/sidebar"
    This reverts commit c61f65c5fffc405ee2bc152f340297ae9d92d2d3, reversing
    changes made to 9e15b2ef1c7d50320cd1d524d916069edc2a35e5.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,48 +1,13 @@
-import { Component, signal } from "@angular/core"
-import { CommonModule } from "@angular/common"
-import { SidebarComponent } from "./components/sidebar/sidebar.component"
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: "app-root",
+  selector: 'app-root',
   standalone: true,
-  imports: [
-    CommonModule,
-    SidebarComponent
-  ],
-  template: `
-    <main>
-      <!-- Sidebar -->
-      <app-sidebar />
-
-      <!-- Main Content -->
-      <!-- <div class="main-content"> -->
-        <!-- Navbar -->
-        <!-- <app-navbar 
-          [activePage]="activePage()" 
-          (pageChange)="setActivePage($event)" />
-
-        @if (activePage() === 'about') {
-          <app-about />
-        } @else if (activePage() === 'resume') {
-          <app-resume />
-        } @else if (activePage() === 'portfolio') {
-          <app-portfolio />
-        } @else if (activePage() === 'blog') {
-          <app-blog />
-        } @else if (activePage() === 'contact') {
-          <app-contact />
-        } -->
-      <!-- </div> -->
-    </main>
-  `,
-  styleUrl: "./app.component.css",
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  activePage = signal<string>("about")
-
-  setActivePage(page: string) {
-    this.activePage.set(page)
-    window.scrollTo(0, 0)
-  }
+  title = 'PortFolio';
 }
-
