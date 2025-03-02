d385a48638c5380724b62e1fc41d03b46335f23e
ishu gupta <guptarshu999@gmail.com>
2025-03-02 17:04:59 +0530

    sidebar completed

---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,22 @@
+import { Component, computed, signal } from '@angular/core';
+import { PortfolioService } from '../../services/portfolio.service';
+import { CommonModule } from '@angular/common';
+import { IonIconComponent } from '../ion-icon/ion-icon.component';
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [CommonModule, IonIconComponent],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.scss',
+  
+})
+export class SidebarComponent {
+  profile = computed(this.portfolioService.getProfile())
+  socialLinks = computed(this.portfolioService.getSocialLinks())
+  isActive = signal(false)
+  constructor(private portfolioService: PortfolioService) {}
+  toggleSidebar() {
+    this.isActive.update((value) => !value)
+  }
+}
